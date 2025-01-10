3704283a9f0a8aa2c8c2371ce9426a2cfbc7d9e3
ezequiaslopesdasilva <wyskzito@gmail.com>
2025-01-10 10:52:40 -0300

    feat: criação de rotas para acessar lista de lojas

---
+++ app/api/store/list/route.ts
@@ -0,0 +1,27 @@
+import { Middleware, controller } from "@/infra/controller";
+import { InternalError } from "@/infra/error";
+import { HttpStatusCode } from "@/infra/httpStatusCode";
+import { Store } from "@/models/store.model";
+import { Store as TStore } from "@prisma/client";
+
+type StoreProps = { stores: TStore[] };
+
+const handleGetStoreList: Middleware<StoreProps> = async (_, ctx, next) => {
+  const stores = await Store.findMany();
+  ctx.stores = stores;
+
+  if (!stores.length) {
+    throw new InternalError({
+      message: "Não foi possível encontrar a lista de lojas.",
+      statusCode: HttpStatusCode.NOT_FOUND,
+    });
+  }
+
+  await next();
+};
+
+const getStoreList: Middleware<StoreProps> = async (_, ctx) => {
+  return Response.json(ctx, { status: HttpStatusCode.FOUND });
+};
+
+export const GET = controller(handleGetStoreList, getStoreList);
