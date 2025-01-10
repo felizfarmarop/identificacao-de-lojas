5c8d55a82d849954f7d5697f6cfe511add81397a
ezequiaslopesdasilva <wyskzito@gmail.com>
2025-01-10 10:55:10 -0300

    feat: adicionar modelos de contato para lojas e validadores de dados

---
+++ models/store.model.ts
@@ -0,0 +1,79 @@
+import database from "@/infra/database";
+import { type Store as TStore } from "@prisma/client";
+import { StoreSchema } from "./store.validator";
+import { ZodError } from "zod";
+import { InternalError } from "@/infra/error";
+import { HttpStatusCode } from "@/infra/httpStatusCode";
+
+export class Store {
+  acronym: TStore["acronym"];
+  tradeName: TStore["tradeName"];
+  companyName: TStore["companyName"];
+  cnpj: TStore["cnpj"];
+
+  constructor(props: TStore) {
+    this.acronym = props.acronym;
+    this.tradeName = props.tradeName;
+    this.companyName = props.companyName;
+    this.cnpj = props.cnpj;
+  }
+
+  static async create(props: TStore) {
+    let result = props;
+
+    try {
+      result = StoreSchema.parse(props);
+    } catch (err) {
+      if (err instanceof ZodError) {
+        throw err;
+      }
+    }
+
+    if (await Store.isUnique(result.acronym, result.cnpj)) {
+      throw new InternalError({
+        message: "This store is not unique",
+        statusCode: HttpStatusCode.UNAUTHORIZED,
+      });
+    }
+
+    await database.store.create({
+      data: {
+        acronym: result.acronym,
+        cnpj: result.cnpj,
+        companyName: result.companyName,
+        tradeName: result.tradeName,
+      },
+    });
+  }
+
+  static async isUnique(acronym: string, cnpj: string) {
+    const withAcronym = await Store.findOneByAcronym(acronym);
+    const withCnpj = await Store.findOneByCpnj(cnpj);
+
+    return !withAcronym && !withCnpj;
+  }
+
+  static async findOneByAcronym(acronym: string) {
+    const result = await database.store.findUnique({
+      where: { acronym: acronym },
+    });
+
+    if (result) return new Store(result);
+    else return undefined;
+  }
+
+  static async findOneByCpnj(cnpj: string) {
+    const result = await database.store.findUnique({
+      where: { cnpj: cnpj },
+    });
+
+    if (result) return new Store(result);
+    else return undefined;
+  }
+  static async findMany() {
+    const result = await database.store.findMany();
+
+    if (result.length) return result.map((store) => new Store(store));
+    else return [];
+  }
+}
---
+++ models/store.validator.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+export const acronymSchema = z
+  .string({ required_error: "ACRONYM é um campo obrigatório" })
+  .min(1, "ACRONYM não pode ser um campo vazio.")
+  .transform((a) => a.toUpperCase());
+
+export const cnpjSchema = z
+  .string({ required_error: "CNPJ é um campo obrigatório." })
+  .min(14, "CNPJ deve conter ao menos 14 caracteres")
+  .max(18, "CNPJ não pode exceder 18 caracteres")
+  .transform((doc) => doc.replace(/\D/g, ""))
+  .refine(
+    (cnpj) => /^\d{14}$/.test(cnpj),
+    "CNPJ deve conter exatamente 14 caracteres."
+  )
+  .transform((cnpj) =>
+    cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
+  );
+
+export const companyNameSchema = z
+  .string({ required_error: "COMPANY_NAME é um campo obrigatório." })
+  .min(1, "COMPANY_NAME não pode ser um campo vazio.")
+  .transform((a) => a.toUpperCase());
+
+export const tradeNameSchema = z
+  .string({ required_error: "TRADE_NAME é um campo obrigatório." })
+  .min(1, "TRADE_NAME não pode ser um campo vazio.")
+  .transform((a) => a.toUpperCase());
+
+export const StoreSchema = z.object({
+  acronym: acronymSchema,
+  cnpj: cnpjSchema,
+  companyName: companyNameSchema,
+  tradeName: tradeNameSchema,
+});
