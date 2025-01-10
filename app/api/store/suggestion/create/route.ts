f9b0482b4916aad54be89bbb88da477e3f6a8d4b
ezequiaslopesdasilva <wyskzito@gmail.com>
2025-01-10 10:52:40 -0300

    feat: criação de rotas para acessar lista de lojas

---
+++ app/api/store/suggestion/create/route.ts
@@ -0,0 +1,15 @@
+import { Middleware, controller } from "@/infra/controller";
+import { HttpStatusCode } from "@/infra/httpStatusCode";
+import { StoreSchema } from "@/models/store.validator";
+// import { StoreSuggestion } from "@/models/storeSuggestion.model";
+import { Store as TStore } from "@prisma/client";
+
+const handleCreateSuggestion: Middleware<TStore> = async (req) => {
+  const data = await req.json();
+  const cleanValues = StoreSchema.parse(data);
+
+  // TODO: implementar logica para validação e criação de sugestão de criação de loja
+  return Response.json(cleanValues, { status: HttpStatusCode.OK });
+};
+
+export const POST = controller(handleCreateSuggestion);
