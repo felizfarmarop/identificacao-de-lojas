d229d5ef17af6abb726227e16a011530941f986c
ezequiaslopesdasilva <wyskzito@gmail.com>
2025-01-13 11:26:06 -0300

    fix: remover temporariamente modelo de sugesão

--- models/storeSuggestion.model.ts
+++ models/storeSuggestion.model.ts
@@ -1,82 +1,77 @@
-import database from "@/infra/database";
-import { type Store as TStore } from "@prisma/client";
-import { StoreSchema } from "./store.validator";
-import { ZodError } from "zod";
+// import database from "@/infra/database";
+// import { type Store as TStore } from "@prisma/client";
+// import { StoreSchema } from "./store.validator";
+// import { ZodError } from "zod";
 
+// type CreateStoreSuggestionProps = Partial<TStore> & {
+//   storeId: string;
+// };
 
-type CreateStoreSuggestionProps = Partial<TStore> & {
-  storeId: string;
-};
+// export class StoreSuggestion {
+//   acronym: TStore["acronym"];
+//   tradeName: TStore["tradeName"];
+//   companyName: TStore["companyName"];
+//   cnpj: TStore["cnpj"];
 
+//   constructor(props: TStore) {
+//     this.acronym = props.acronym;
+//     this.tradeName = props.tradeName;
+//     this.companyName = props.companyName;
+//     this.cnpj = props.cnpj;
+//   }
 
-export class StoreSuggestion {
-  acronym: TStore["acronym"];
-  tradeName: TStore["tradeName"];
-  companyName: TStore["companyName"];
-  cnpj: TStore["cnpj"];
+//   static async create({ storeId, ...props }: CreateStoreSuggestionProps) {
 
-  constructor(props: TStore) {
-    this.acronym = props.acronym;
-    this.tradeName = props.tradeName;
-    this.companyName = props.companyName;
-    this.cnpj = props.cnpj;
-  }
+//     let result = props;
 
+//     try {
+//       result = StoreSchema.parse(props);
+//     } catch (err) {
+//       if (err instanceof ZodError) {
+//         throw err;
+//       }
+//     }
 
-  static async create({ storeId, ...props }: CreateStoreSuggestionProps) {
+//     await database.storeSuggestion.create({
+//       data: {
+//         storeId,
+//         acronym: result.acronym,
+//         cnpj: result.cnpj,
+//         companyName: result.companyName,
+//         tradeName: result.tradeName,
+//       },
+//     });
+//   }
 
-    let result = props;
+//   static async isUnique(acronym: string, cnpj: string) {
+//     const withAcronym = await Store.findOneByAcronym(acronym);
+//     const withCnpj = await Store.findOneByCpnj(cnpj);
 
-    try {
-      result = StoreSchema.parse(props);
-    } catch (err) {
-      if (err instanceof ZodError) {
-        throw err;
-      }
-    }
+//     return !withAcronym && !withCnpj;
+//   }
 
+//   static async findOneByAcronym(acronym: string) {
+//     const result = await database.storeSuggestion.findUnique({
+//       where: { acronym: acronym },
+//     });
 
-    await database.storeSuggestion.create({
-      data: {
-        storeId,
-        acronym: result.acronym,
-        cnpj: result.cnpj,
-        companyName: result.companyName,
-        tradeName: result.tradeName,
-      },
-    });
-  }
+//     if (result) return new Store(result);
+//     else return undefined;
+//   }
 
+//   static async findOneByCpnj(cnpj: string) {
+//     const result = await database.storeSuggestion.findUnique({
+//       where: { cnpj: cnpj },
+//     });
 
-  static async isUnique(acronym: string, cnpj: string) {
-    const withAcronym = await Store.findOneByAcronym(acronym);
-    const withCnpj = await Store.findOneByCpnj(cnpj);
+//     if (result) return new Store(result);
+//     else return undefined;
+//   }
 
-    return !withAcronym && !withCnpj;
-  }
+//   static async findMany() {
+//     const result = await database.storeSuggestion.findMany();
 
-  static async findOneByAcronym(acronym: string) {
-    const result = await database.storeSuggestion.findUnique({
-      where: { acronym: acronym },
-    });
-
-    if (result) return new Store(result);
-    else return undefined;
-  }
-
-  static async findOneByCpnj(cnpj: string) {
-    const result = await database.storeSuggestion.findUnique({
-      where: { cnpj: cnpj },
-    });
-
-    if (result) return new Store(result);
-    else return undefined;
-  }
-
-  static async findMany() {
-    const result = await database.storeSuggestion.findMany();
-
-    if (result.length) return result.map((store) => new Store(store));
-    else return [];
-  }
-}
+//     if (result.length) return result.map((store) => new Store(store));
+//     else return [];
+//   }
+// }
