3527c6f4c7c45a7bf411617a090461ffae79660d
ezequiaslopesdasilva <wyskzito@gmail.com>
2025-01-15 11:42:18 -0300

    feat: adicionar validação para campo de sugestões em loja

--- models/store.model.ts
+++ models/store.model.ts
@@ -1,24 +1,32 @@
 import database from "@/infra/database";
-import { type Store as TStore } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { StoreSchema } from "./store.validator";
 import { ZodError } from "zod";
 import { InternalError } from "@/infra/error";
 import { HttpStatusCode } from "@/infra/httpStatusCode";
 
-export class Store {
-  acronym: TStore["acronym"];
-  tradeName: TStore["tradeName"];
-  companyName: TStore["companyName"];
-  cnpj: TStore["cnpj"];
+type StoreWithSuggestions = Prisma.StoreGetPayload<{
+  include: {
+    suggestions: true;
+  };
+}>;
 
-  constructor(props: TStore) {
+export class Store {
+  acronym: StoreWithSuggestions["acronym"];
+  tradeName: StoreWithSuggestions["tradeName"];
+  companyName: StoreWithSuggestions["companyName"];
+  cnpj: StoreWithSuggestions["cnpj"];
+  suggestions?: StoreWithSuggestions["suggestions"];
+
+  constructor(props: StoreWithSuggestions) {
     this.acronym = props.acronym;
     this.tradeName = props.tradeName;
     this.companyName = props.companyName;
     this.cnpj = props.cnpj;
+    this.suggestions = props.suggestions;
   }
 
-  static async create(props: TStore) {
+  static async create(props: StoreWithSuggestions) {
     let result = props;
 
     try {
@@ -54,10 +62,9 @@ export class Store {
   }
 
   static async findOneByAcronym(acronym: string) {
-
     const result = await database.store.findFirst({
-
       where: { acronym: acronym },
+      include: { suggestions: true },
     });
 
     if (result) return new Store(result);
@@ -65,21 +72,18 @@ export class Store {
   }
 
   static async findOneByCpnj(cnpj: string) {
-
     const result = await database.store.findFirst({
-
       where: { cnpj: cnpj },
+      include: { suggestions: true },
     });
 
     if (result) return new Store(result);
     else return undefined;
   }
   static async findMany() {
-
     const result = await database.store.findMany({
       include: { suggestions: true },
     });
-
 
     if (result.length) return result.map((store) => new Store(store));
     else return [];
