4c1e342c7ff34d8224b5a2167f5625ba832b479b
ezequiaslopesdasilva <wyskzito@gmail.com>
2025-01-16 11:47:45 -0300

    fix: adaptar tabelas para sugestão de alteração e votos

--- components/Table/CustomTable.tsx
+++ components/Table/CustomTable.tsx
@@ -1,18 +1,19 @@
 import { Edit } from "lucide-react";
-import { FutureFeaturesModal } from "../FutureFeaturesModal";
 import { Button } from "../ui/button";
 import { TableCell } from "../ui/table";
 import { StoreWithSuggestion } from "../utils";
 import { HoverSuggestionCard } from "./HoverSuggestionCard";
 import { Store } from "@prisma/client";
+import { SuggestionModal } from "../SuggestionModal";
 
 type CustomTableProps = {
   stores: StoreWithSuggestion[];
   action: (value: string) => void;
+  updateList: () => void;
   field: keyof Omit<Store, "id">;
 };
 
-const FieldMap = {
+export const FieldMap = {
   acronym: "Sigla",
   tradeName: "Nome Fantasia",
   companyName: "Razão Social",
@@ -23,9 +24,9 @@ export default function CustomTable(props: CustomTableProps) {
   const [field, fieldDescription] = [props.field, FieldMap[props.field]];
 
   return props.stores.map((store, index) => {
-    const { suggestions, tradeName } = store;
-    const fieldValue = store[field];
+    const { suggestions, id } = store;
 
+    const fieldValue = store[field];
     const tooltipAction = field === "acronym" ? "print" : "copy";
 
     return (
@@ -33,21 +34,24 @@ export default function CustomTable(props: CustomTableProps) {
         <div className="flex items-center space-x-2 group">
           <HoverSuggestionCard
             addToPrintList={props.action}
+            updateList={props.updateList}
             suggestions={suggestions}
             action={tooltipAction}
             value={fieldValue}
             field={field}
           />
           <span className="opacity-0 group-hover:opacity-100 transition-opacity">
-            <FutureFeaturesModal
-              actionType="edit"
-              fieldName={fieldDescription}
-              companyName={tradeName}
+            <SuggestionModal
+              storeId={id}
+              field={field}
+              fieldPlaceholder={fieldDescription}
+              currentValue={fieldValue}
+              updateList={props.updateList}
             >
               <Button variant="ghost" size="icon" className="h-8 w-8 p-0">
                 <Edit className="h-4 w-4" />
               </Button>
-            </FutureFeaturesModal>
+            </SuggestionModal>
           </span>
         </div>
       </TableCell>
